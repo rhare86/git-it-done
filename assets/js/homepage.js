1402acc369bbf194deded1109d48caf4dcf5872f
rhare86 <rhare86@gmail.com>
2022-04-17 23:09:22 -0500

    lesson 1

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    // format the github api ulr
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos();
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+getUserRepos("rhare86");
